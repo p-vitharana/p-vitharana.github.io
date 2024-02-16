a4c00cbf85197dfa38540bee31a36584f026bf16
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-16 20:39:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"
-  
-window.start.init({Palette:"palette4",Mode:"banner bottom",Theme:"classic",ButtonText:"Got It!",LinkText:"Learn more here!",Time:"5",})
+
