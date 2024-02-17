5a5bc2271c3aeee9a4f03ce7d0ed3bffb8125546
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-17 14:38:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,16 @@ function darklightswitch() {
   var element3 = document.querySelector("footer");
   element3.classList.toggle("light-mode-shadow2");
 
-  var element4 = document.querySelector(".topnav a:hover, .topnav a:active");
+  var element4 = document.querySelector(".topnav a:hover");
   element4.classList.toggle("light-mode2");
   
   var element5 = document.querySelectorAll(".topnav a");
   element5.forEach(function(element) {
     element.classList.toggle("light-mode3");
   });
+
+  var element6 = document.querySelector(".topnav a:active");
+  element6.classList.toggle("light-mode4");
 }
 
 
