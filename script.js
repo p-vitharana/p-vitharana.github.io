6c964205ef1b87eb39bdc535d6ab20a4283f89da
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-17 11:16:49 +0000

    fix: fix syntax errors

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ function darklightswitch() {
   var element3 = document.querySelector("footer");
   element3.classList.toggle("light-mode-shadow2");
 
-  var element4 = document.querySelector(".topnav a:hover, a:active");
+  var element4 = document.querySelector(".topnav a:hover, .topnav a:active");
   element4.classList.toggle("light-mode2");
   
-  var element5 = document.querySelector(".topnav a:");
-  element5.classList.toggle("light-mode3");
-  }
+  var element5 = document.querySelectorAll(".topnav a");
+  element5.forEach(function(element) {
+    element.classList.toggle("light-mode3");
+  });
+}
+
 
