2b5277ca663993883a39399674a82eb721c36ec6
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-17 11:10:00 +0000

    fix: combined conds

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function darklightswitch() {
-  var elements = document.querySelectorAll('*');
+  var elements = document.querySelectorAll("*");
   elements.forEach(function(element) {
     element.classList.toggle("light-mode");
   });
 
-  var element2 = document.querySelector('.topnav');
+  var element2 = document.querySelector(".topnav");
   element2.classList.toggle("light-mode-shadow1");
-
-  var element3 = document.querySelector('footer');
+  
+  var element3 = document.querySelector("footer");
   element3.classList.toggle("light-mode-shadow2");
-}
+
+  var element4 = document.querySelector(".topnav a:hover, a:active");
+  element4.classList.toggle("light-mode2");
+  
+  var element5 = document.querySelector(".topnav a:");
+  element5.classList.toggle("light-mode3");
+  }
+
