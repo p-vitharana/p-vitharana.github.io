175f0784ff95ec459f5a4464ee54992e3b7886ec
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-17 10:02:16 +0000

    feat: add switch for dark mode

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function darklightswitch() {
+  var element = document.*;
+  element.classList.toggle("light-mode");
+  var element2 = document.topnav;
+  element2.classList.toggle("light-mode-shadow1")
+  var element3 = document.footer;
+  element3.classList.toggle("light-mode-shadow2")
+}
