abbbe24962b0ec21a0a74b0884553dbc8cc99d37
Pamoda Waragoda Vitharana <pamodawv@gmail.com>
2024-02-18 13:05:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const toggleSwitch = document.getElementById("modeToggle");
 toggleSwitch.addEventListener('change', function() {
   if(this.checked) {
     document.body.classList.add("light-mode");
-    document.body.classList.add("lightmode2");
+    document.*.classList.add("lightmode2");
     document.querySelector(".topnav").classList.add("lightmode3");
     const topNavLinks = document.querySelectorAll(".topnav a");
     topNavLinks.forEach(link => link.classList.add("lightmode4"));
@@ -13,7 +13,7 @@ toggleSwitch.addEventListener('change', function() {
     topNavActive.forEach(link => link.classList.add("lightmode6"));
   } else {
     document.body.classList.remove('light-mode');
-    document.body.classList.remove("lightmode2");
+    document.*.classList.remove("lightmode2");
     document.querySelector(".topnav").classList.remove("lightmode3");
     const topNavLinks = document.querySelectorAll(".topnav a");
     topNavLinks.forEach(link => link.classList.remove("lightmode4"));
